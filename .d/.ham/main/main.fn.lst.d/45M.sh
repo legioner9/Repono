fc6ab575af054faf756558f4b8ac3ff668fd8068
[alt]legioner9 <legioner9@inbox.ru>
2024-01-16 00:19:28 +0700

    <> ##

--- .d/.ham/main/main.fn.lst.d/45M.sh
+++ .d/.ham/main/main.fn.lst.d/45M.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-arr_res=("$(find ${REPO_PATH}/Repono -not -path "${REPO_PATH}/Repono/.git/*" -type f -size +45M)")
+arr_res=()
 
-if [ -n "$(find ${REPO_PATH}/Repono -not -path "${REPO_PATH}/Repono/.git/*" -type f -size +45M)" ]; then
-# if [ -n "$(find ${REPO_PATH}/Repono -type f -size +45M)" ]; then
+IFS=$'\n'
+arr_res=($(find ${REPO_PATH}/Repono -not -path "${REPO_PATH}/Repono/.git/*" -type f -size +${1}M))
+IFS=$' \t\n'
+
+# IFS=$'\n'
+if [ -n "${arr_res[0]}" ]; then
+# if [ -n "$(find ${REPO_PATH}/Repono -type f -size +${1}M)" ]; then
     parr3e_ arr_res
-    plt_info "in ${REPO_PATH}/Repono/.d/.ham/main/main.fn.lst.d/45M.sh : TRUE_EXEC : find ${REPO_PATH}/Repono -type f -size -not -path ${REPO_PATH}/Repono/.git/* +45M : return 1"
+    plt_info "in ${REPO_PATH}/Repono/.d/.ham/main/main.fn.lst.d/${1}M.sh : TRUE_EXEC : find ${REPO_PATH}/Repono -type f -size -not -path ${REPO_PATH}/Repono/.git/* +45M : return 1"
     return 1
 fi
+# IFS=$' \t\n'
 
 return 0
